e2bee00443c7b49efb16fec9a4a6091a4596a0c5
raindy <zhangyouce@gmail.com>
2015-12-04 22:29:36 +0800

    change back mongodb

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-process.env.MONGO_URL = 'mongodb://localhost:27017/test';
-//process.env.MONGO_URL = 'mongodb://uAxwrYvVoXJzK45c:pCPGLnufBavID0HQb@10.10.72.139:27017/ZedRHpOCXtz8ik1W';
+//process.env.MONGO_URL = 'mongodb://localhost:27017/test';
+process.env.MONGO_URL = 'mongodb://uAxwrYvVoXJzK45c:pCPGLnufBavID0HQb@10.10.72.139:27017/ZedRHpOCXtz8ik1W';
 process.env.PORT = 8080;
 require('./app/core/mongoose')
 require('./app/core/router')
