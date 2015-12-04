dc9cb2435ce1919d6b80a2b441d2a1bf0cb987f3
raindy <zhangyouce@gmail.com>
2015-12-04 20:26:46 +0800

    change the mongodb url

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-process.env.MONGO_URL = 'mongodb://localhost:27017/test';
+//process.env.MONGO_URL = 'mongodb://localhost:27017/test';
+process.env.MONGO_URL = 'mongodb://user:pwd@localhost:27020/testing';
 process.env.PORT = 8080;
 require('./app/core/mongoose')
 require('./app/core/router')
